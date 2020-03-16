963c0629f33a1bc871b27f644af54c8dced623ba
amantemiguel <61431200+amantemiguel@users.noreply.github.com>
2020-03-16 12:30:47 -0700

    Updates

---
+++ precache-manifest.7c994670c99ab0002eb9a96e37e0f446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de08bfeaec1d6cdab3a781d181b61b87",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "806b677e914e4356343c",
+    "url": "/pokedex/static/css/main.8ef6a98d.chunk.css"
+  },
+  {
+    "revision": "d3c4ad99eacd37f7aeeb",
+    "url": "/pokedex/static/js/2.9962af93.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedex/static/js/2.9962af93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806b677e914e4356343c",
+    "url": "/pokedex/static/js/main.ba75b7e9.chunk.js"
+  },
+  {
+    "revision": "e9c8d32f045e9b89c48b",
+    "url": "/pokedex/static/js/runtime-main.54804aa2.js"
+  }
+]);
